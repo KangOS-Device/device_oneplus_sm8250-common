e5f951f9a779835544aa540ae217906ce1e5ce87
chandu078 <chandudyavanapelli03@gmail.com>
2021-04-28 10:17:57 +0530

    sm8250-common: Set processor prop for about phone
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -340,6 +340,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# KangOS specific properties
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.kangos.cpu=SDM865
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
