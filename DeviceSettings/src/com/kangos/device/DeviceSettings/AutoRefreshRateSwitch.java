timmah1984/device_oneplus_sm8250-common-1

sm8250-common: DeviceSettings: Introducing Screen refresh rate * chandu078: Adapted to OnePlus sm8250-common
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

DeviceSettings/src/org/evolution/device/DeviceSettings/AutoRefreshRateSwitch.java
@@ -1,0 +1,34 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceSettings;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import org.evolution.device.DeviceSettings.DeviceSettings;
+
+public class AutoRefreshRateSwitch {
+
+    public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_AUTO_REFRESH_RATE;
+
+    public AutoRefreshRateSwitch() { }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), SETTINGS_KEY, 1) == 1;
+    }
+}

DeviceSettings/src/org/evolution/device/DeviceSettings/RefreshRateSwitch.java
@@ -1,0 +1,41 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceSettings;
+
+import android.content.Context;
+import android.provider.Settings;
+
+public class RefreshRateSwitch {
+
+    public RefreshRateSwitch() { }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        boolean peak = Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, 120f) == 120f;
+        boolean min = Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 60f) == 120f;
+        return peak && min;
+    }
+
+    public static void setPeakRefresh(Context context, boolean enabled) {
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, enabled ? 120f : 60f);
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, enabled ? 120f : 60f);
+    }
+}

DeviceSettings/src/org/evolution/device/DeviceSettings/RefreshRateTileService.java
@@ -1,0 +1,91 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+
+@TargetApi(24)
+public class RefreshRateTileService extends TileService {
+    private boolean enabled = false;
+    private boolean autoRefreshEnabled;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        autoRefreshEnabled = Settings.System.getInt(this.getContentResolver(),
+                AutoRefreshRateSwitch.SETTINGS_KEY, 1) == 1;
+        if (autoRefreshEnabled) {
+            getQsTile().setState(Tile.STATE_UNAVAILABLE);
+        } else {
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_120 : R.drawable.ic_refresh_tile_60));
+            getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        if (!autoRefreshEnabled) {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+            sharedPrefs.edit().putBoolean(DeviceSettings.KEY_REFRESH_RATE, enabled ? false : true).commit();
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.PEAK_REFRESH_RATE, enabled ? 60f : 120f);
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.MIN_REFRESH_RATE, enabled ? 60f : 120f);
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_60 : R.drawable.ic_refresh_tile_120));
+            getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+            getQsTile().updateTile();
+        }
+    }
+}

KangOS-Device/device_oneplus_sm8250-common

Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>

DeviceSettings/src/com/kangos/device/DeviceSettings/AutoRefreshRateSwitch.java
@@ -1,0 +1,34 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.kangos.device.DeviceSettings;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import com.kangos.device.DeviceSettings.DeviceSettings;
+
+public class AutoRefreshRateSwitch {
+
+    public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_AUTO_REFRESH_RATE;
+
+    public AutoRefreshRateSwitch() { }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), SETTINGS_KEY, 1) == 1;
+    }
+}

DeviceSettings/src/com/kangos/device/DeviceSettings/RefreshRateSwitch.java
@@ -1,0 +1,41 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.kangos.device.DeviceSettings;
+
+import android.content.Context;
+import android.provider.Settings;
+
+public class RefreshRateSwitch {
+
+    public RefreshRateSwitch() { }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        boolean peak = Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, 120f) == 120f;
+        boolean min = Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 60f) == 120f;
+        return peak && min;
+    }
+
+    public static void setPeakRefresh(Context context, boolean enabled) {
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, enabled ? 120f : 60f);
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, enabled ? 120f : 60f);
+    }
+}

DeviceSettings/src/com/kangos/device/DeviceSettings/RefreshRateTileService.java
@@ -1,0 +1,91 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.kangos.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+
+@TargetApi(24)
+public class RefreshRateTileService extends TileService {
+    private boolean enabled = false;
+    private boolean autoRefreshEnabled;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        autoRefreshEnabled = Settings.System.getInt(this.getContentResolver(),
+                AutoRefreshRateSwitch.SETTINGS_KEY, 1) == 1;
+        if (autoRefreshEnabled) {
+            getQsTile().setState(Tile.STATE_UNAVAILABLE);
+        } else {
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_120 : R.drawable.ic_refresh_tile_60));
+            getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        if (!autoRefreshEnabled) {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+            sharedPrefs.edit().putBoolean(DeviceSettings.KEY_REFRESH_RATE, enabled ? false : true).commit();
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.PEAK_REFRESH_RATE, enabled ? 60f : 120f);
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.MIN_REFRESH_RATE, enabled ? 60f : 120f);
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_60 : R.drawable.ic_refresh_tile_120));
+            getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+            getQsTile().updateTile();
+        }
+    }
+}